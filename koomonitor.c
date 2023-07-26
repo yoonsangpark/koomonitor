7a0866383a56e53ed432c66fc8137cb9befd564b
yoonsangpark <yoonsangpark@gmail.com>
2023-07-26 12:36:22 -0400

    gpio control

--- koomonitor.c
+++ koomonitor.c
@@ -12,26 +12,32 @@
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 
+#include <linux/gpio.h>
+#include <plat/nvt-gpio.h>
+
 #include "koomonitor.h"
 
 #define DRIVER_NAME "koomonitor"
 #define DRIVER_VERSION "0.1"
 
 
+//#define MGPIO P_GPIO(11)
+#define MGPIO	43 
+
 static int koo_misc_open(struct inode *inode, struct file *file)
 {
-    pr_info("KOO misc device open\n");
+	pr_info("KOO misc device open\n");
 
-    return 0;
+	return 0;
 }
 /*
 ** This function will be called when we close the Misc Device file
 */
 static int koo_misc_close(struct inode *inodep, struct file *filp)
 {
-    pr_info("KOO misc device close\n");
+	pr_info("KOO misc device close\n");
 
-    return 0;
+	return 0;
 }
 /*
 ** This function will be called when we write the Misc Device file
@@ -39,9 +45,9 @@ static int koo_misc_close(struct inode *inodep, struct file *filp)
 static ssize_t koo_misc_write(struct file *file, const char __user *buf,
                size_t len, loff_t *ppos)
 {
-    pr_info("KOO misc device write\n");
+	pr_info("KOO misc device write\n");
     
-    return len; 
+	return len; 
 }
  
 /*
@@ -50,29 +56,43 @@ static ssize_t koo_misc_write(struct file *file, const char __user *buf,
 static ssize_t koo_misc_read(struct file *filp, char __user *buf,
                     size_t count, loff_t *f_pos)
 {
-    pr_info("KOO misc device read\n");
- 
-    return 0;
+	uint32_t value = 0;
+	pr_info("KOO misc device read\n");
+
+	gpio_direction_input(MGPIO); 
+	value = gpio_get_value(MGPIO);
+	pr_info("MGPIO = %d\n", value);
+
+	value = gpio_to_irq(MGPIO);
+	pr_info("MGPIO IRQ = %d\n", value);
+
+	return 0;
 }
 
 //File operation structure
 static const struct file_operations koo_misc_fops = {
-    .owner          = THIS_MODULE,
-    .write          = koo_misc_write,
-    .read           = koo_misc_read,
-    .open           = koo_misc_open,
-    .release        = koo_misc_close,
+	.owner          = THIS_MODULE,
+	.write          = koo_misc_write,
+	.read           = koo_misc_read,
+	.open           = koo_misc_open,
+	.release        = koo_misc_close,
 };
 
 //Misc device structure
 struct miscdevice koo_misc_device = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = "koo_misc",
-    .fops = &koo_misc_fops,
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "koo_misc",
+	.fops = &koo_misc_fops,
 };
 
 static int koomonitor_probe(struct platform_device *dev)
 {
+	int err;
+	err = gpio_request(MGPIO, "gpio_test");
+
+	if (err)
+		printk(KERN_ERR "#### failed to request MGPIO\n");
+	
 	pr_info("koomonitor probe\n");
 	
 	return 0;
@@ -80,6 +100,8 @@ static int koomonitor_probe(struct platform_device *dev)
 
 static int koomonitor_remove(struct platform_device *dev)
 {
+	gpio_free(MGPIO);
+
 	pr_info("koomonitor removed\n");
 	
 	return 0;
